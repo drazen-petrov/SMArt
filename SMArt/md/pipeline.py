1f1fd979129662a750b869b77966a52cb915b23d
dpetrov <drazen.petrov@boku.ac.at>
2022-03-28 19:38:21 +0200

    pipeline: slurm_job fix; run_initial_FE documentation

--- SMArt/md/pipeline.py
+++ SMArt/md/pipeline.py
@@ -2,14 +2,24 @@ from SMArt.incl import os, np
 from SMArt.md.ana.incl import get_lset, fix_float, Real
 
 def get_slurm_job_settings(**slurm_kw):
+    """
+    :param slurm_kw: key value dictionary that will add the following lines: '#SBATCH -{:} {:}\n'
+    :return: header string
+    """
     s = '#!/bin/bash\n'
     for item in slurm_kw.items():
         s+= '#SBATCH -{:} {:}\n'.format(*item)
     return s + '\n'
 
-def gen_slurm_job(f_path, commands, **kwargs):
+def gen_slurm_job(f_path, commands, **slurm_kw):
+    """
+    :param f_path: run file
+    :param commands: commands to be executed in the run file
+    :param slurm_kw: additional slurm settings (see  get_slurm_job_settings function)
+    :return: None
+    """
     f = open(f_path, 'w')
-    f.write(get_slurm_job_settings(**kwargs))
+    f.write(get_slurm_job_settings(**slurm_kw))
     for comm in commands:
         if not comm.endswith('\n'):
             comm+='\n'
@@ -23,7 +33,18 @@ def prep_pdb(pdb):
 def eq(top, conf, md_in, eq_jobs):
     pass
 
-def run_intial_FE(t_eq, t_sim, LPs = None,  **kwargs):
+def run_intial_FE(t_eq, t_sim, LPs = None, flag_run_eq=True,  **kwargs):
+    """
+    gets a simulation set for FE calculations
+    :param t_eq: equilibation time [ns]
+    :param t_sim: production run time [ns]
+    :param LPs: list of lambda points
+    :param flag_run_eq: run all eq simulations in one job
+    :param kwargs:
+        N_lam: number of equidistant lambda points (for LPs)
+    :return:
+        set of simulations (dictionary)
+    """
     if LPs is None:
         N_lam = kwargs.get('N_lam', 11)
         LPs = get_lset(np.linspace(0, 1., N_lam))
@@ -32,7 +53,10 @@ def run_intial_FE(t_eq, t_sim, LPs = None,  **kwargs):
     current_eq_sim_id = 0
     for i,l in enumerate(LPs):
         if i != len(LPs) - 1:
-            new_sim = dict(eq=True, l=l, t=t_eq, sub=(c+1, ), run=(c+2, ))
+            if flag_run_eq:
+                new_sim = dict(eq=True, l=l, t=t_eq, sub=(c+1, ), run=(c+2, ))
+            else:
+                new_sim = dict(eq=True, l=l, t=t_eq, sub=(c+1, c+2), run=())
             sim_set.append(new_sim)
         else:
             new_sim = dict(eq=True, l=l, t=t_eq, sub=(c+1, ), run=())
